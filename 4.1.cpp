fa9bceb68069e98b6c7387a18fb8b7dd203d1af4
s27151 <115358243+pjwstkGabe@users.noreply.github.com>
2022-12-17 16:17:51 +0100

    Update 4.1.cpp

--- 4.1.cpp
+++ 4.1.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int x;
+    int y;
+    int tbl[] = {0};
+    cout << "Podaj dlugosc tablicy:";
+    cin >> x;
+    cout << endl;
+
+    for (int i = 0; i < x; i++) {
+        cout << "Podaj liczbe znajdujaca sie na pozycji :" << i + 1 << endl;
+        cin >> tbl[i];
+    }
+
+
+  
+    for (int i = 0; i < x; i++) {
+        cout << tbl[i];
+    }
+    return 0;
+}

1e96afe749b25c29eb08881936034c407878d662
s27151 <115358243+pjwstkGabe@users.noreply.github.com>
2023-01-11 15:31:56 +0100

    Update 6.1.cpp

--- 6.1.cpp
+++ 6.1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Twoja liczba: ";
+    int n;
+    cin >> n;
+    
+    int p = 0; //liczby podzielne przez 5 niepodzielne przez 3
+    
+    for (int i = 1; i <= n; i++){
+        
+        if ((i % 5) == 0 && (i % 3) != 0)
+            p++;
+    } 
+    cout << p;
+    return 0;
+}
 

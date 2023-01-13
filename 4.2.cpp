e08277363c98e7b36e8cecc7e3ba56ab99a89e3c
s27151 <115358243+pjwstkGabe@users.noreply.github.com>
2023-01-13 14:33:17 +0100

    Update 4.2.cpp

--- 4.2.cpp
+++ 4.2.cpp
@@ -2,46 +2,46 @@
 using namespace std;
 
 int main()
-{   
+{
     int x;
     cout<<"Podaj rozmiar tablicy: ";
     cin>>x;
- 
+
     int arr[x];
- 
+
     for(int i=0; i<x; i++)
     {
         cout<<"Podaj element "<<i+1<<" tablicy: ";
         cin>>arr[i];
     }
-    
+
     cout << endl << "Twoja tablica to: ";
-	for (int z = 0; z < x; z++) {
+    for (int z = 0; z < x; z++) {
 
-		cout << arr[z];
+        cout << arr[z] << " ";
 
-	}
+    }
     cout << endl;
-    
+
     int odp = 0;
     int maxcount = 0;
-	
-	for (int i = 0; i < x; i++) {
-		int count = 0;
-		
-		for (int j = 0; j < x; j++) {
-		    
-			if (arr[i] == arr[j])
-				count++;
-		}
 
-		if (count > maxcount) {
-		    
-			maxcount = count;
-			odp = arr[i];
-			
-		}
-	}
-	cout << "najczęściej występujący element w twojej tablicy liczb całkowitych to: " << odp;
-	
+    for (int i = 0; i < x; i++) {
+        int count = 0;
+
+        for (int j = 0; j < x; j++) {
+
+            if (arr[i] == arr[j])
+                count++;
+        }
+
+        if (count > maxcount) {
+
+            maxcount = count;
+            odp = arr[i];
+
+        }
+    }
+    cout << "najczesciej wystepujacy element w twojej tablicy liczb calkowitych to: " << odp;
+
 }

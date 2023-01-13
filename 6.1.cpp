4f7386e06269f6136f94e0912ab465a63dc252bb
s27151 <115358243+pjwstkGabe@users.noreply.github.com>
2023-01-13 14:43:15 +0100

    Update 6.1.cpp

--- 6.1.cpp
+++ 6.1.cpp
@@ -7,15 +7,14 @@ int main()
     cout<<"Twoja liczba: ";
     int n;
     cin >> n;
-    
+
     int p = 0; //liczby podzielne przez 5 niepodzielne przez 3
-    
+
     for (int i = 1; i <= n; i++){
-        
+
         if ((i % 5) == 0 && (i % 3) != 0)
             p++;
-    } 
-    cout << p;
+    }
+    cout << "(liczby podzielne przez 5 niepodzielne przez 3 nie wieksze niz twoja liczba) Odp: " << p;
     return 0;
 }
-

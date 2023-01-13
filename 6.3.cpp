168de8d04b30c26a93e81a9a62aedda8f8c750f5
s27151 <115358243+pjwstkGabe@users.noreply.github.com>
2023-01-13 14:48:40 +0100

    Update 6.3.cpp

--- 6.3.cpp
+++ 6.3.cpp
@@ -10,52 +10,52 @@ int main()
     int wv;
     int variuable;
     string variuable2;
-    
-    cout << "Podaj wielkość wektora (liczba studentów): ";
+
+    cout << "Podaj wielkosc wektora (liczba studentow): ";
     cin >> wv;
     int x = 1;
     while ( vec.size() != wv ) {
-        
+
         cout << endl << "Dane dla: " << x << " Studenta; Podaj numer indeksu: ";
         cin >> variuable;
         vec.push_back(variuable);
-        
+
         cout << endl <<"Dane dla: " << x << " Studenta; Podaj imie studenta: ";
         cin >> variuable2;
         imie.push_back(variuable2);
-        
+
         cout << endl <<"Dane dla: " << x << " Studenta; Podaj nazwisko studenta: ";
         cin >> variuable2;
         nazwisko.push_back(variuable2);
-        
+
         x++;
     }
-    
+
     int temporary;
     string temporary2;
-    
+
     for (int i = 0; i < vec.size(); i++){
         for (int j = i + 1; j < vec.size(); j++){
-            
+
             if (vec[j] < vec[i]){
                 temporary = vec[i];
                 vec[i] = vec[j];
                 vec[j] = temporary;
-                
+
                 temporary2 = imie[i];
                 imie[i] = imie[j];
                 imie[j] = temporary2;
-                
+
                 temporary2 = nazwisko[i];
                 nazwisko[i] = nazwisko[j];
                 nazwisko[j] = temporary2;
-                
+
             }
         }
     }
-  
+
     for (int i = 0; i < vec.size(); i++){
-        cout << vec[i] << " " << imie[i] << " " << nazwisko[i] << "\t";
+        cout << vec[i] << " " << imie[i] << " " << nazwisko[i] << endl;
     }
     return 0;
 }

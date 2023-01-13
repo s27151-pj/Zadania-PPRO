22440f2147eb84002a88c90b215ff5a6b01f2cde
s27151 <115358243+pjwstkGabe@users.noreply.github.com>
2023-01-13 14:37:28 +0100

    Update 5.1.cpp

--- 5.1.cpp
+++ 5.1.cpp
@@ -7,40 +7,38 @@ int main()
     vector <int> vec;
     int wv;
     int variuable;
-    
-    cout << "Podaj wielkość wektora: ";
+
+    cout << "Podaj wielkosc wektora: ";
     cin >> wv;
-    
+
     while ( vec.size() != wv ) {
-        
+
         cout << "Podaj zmienna: ";
         cin >> variuable;
         vec.push_back(variuable);
     }
-    
+
     cout << "Twoj wektor: ";
     for (int i = 0; i < vec.size(); i++){
-        
         cout << vec[i] << " ";
-        
     }
     cout << endl;
-    
-    int a = 0;
-	int b = 0;
-	int c = 0;
-	
-	for (int t = 0; t < vec.size(); t++) {
 
-		a = vec[t];
-		if (a >= b) {
-		    c = t;
-			b = a;
-		}
-	}
-	
-	cout << "Wartość elementu maksymalnego wektora: " << b << endl;
-	cout << "Numer indeksu elementu maksymalnego: " << c+1;
-    
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    for (int t = 0; t < vec.size(); t++) {
+
+        a = vec[t];
+        if (a >= b) {
+            c = t;
+            b = a;
+        }
+    }
+
+    cout << "Wartosc elementu maksymalnego wektora: " << b << endl;
+    cout << "Numer indeksu elementu maksymalnego: " << c+1;
+
     return 0;
 }
